b2f2c41274fb366be04e960af35cedb0435cb9ac
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-08 22:01:34 -0400

    generate & display productNames && more styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const companies = new Array(50).fill('').map(_ =>{
     return(faker.company.companyName())
 })
 
+const productMap = companies.reduce((acc, company)=>{
+    acc[company] = new Array(1 + faker.random.number(2)).fill('-').map(_ =>{
+        return(faker.commerce.productName())
+    })
+    return acc;
+},{})
+
+console.log(productMap)
+
 
 const companyList = document.querySelector('#company-List')
 
@@ -12,6 +21,15 @@ const render = () =>{
         ${companies.map(company =>`
             <li>
                 ${company}
+                <ul>
+                    ${productMap[company].map(product =>{
+                        return(`
+                            <li>
+                                ${product}
+                            </li>
+                        `)
+                    }).join('')}
+                </ul>
             </li>
         `).join('')}
     `
